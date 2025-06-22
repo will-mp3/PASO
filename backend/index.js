bad1bcf6054d72bb9bd63d1cfb0e3cea04174215
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-22 01:27:10 -0400

    modified package.json and added imports to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 // backend/index.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
